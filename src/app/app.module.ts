9747678a2167d9472e120ef938212324104220d6
kiratsunami7 <kiratsunami48@gmail.com>
2023-01-12 19:25:44 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ItemListComponent } from './item-list/item-list.component';
 import { HeaderComponent } from './header/header.component';
+import { StoreModule } from '@ngrx/store';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemListComponent,
-    HeaderComponent
+    HeaderComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
